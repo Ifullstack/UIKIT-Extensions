a026739d869ca78fbfda09a93eb4c7e9fe0c5b92
Cané Allesta <idevapps19@gmail.com>
2020-11-03 14:58:26 +0100

    Primer Commit

---
+++ UiExtensions.swift
@@ -0,0 +1,261 @@
+//
+//  AnchorExtension.swift
+//  TestComest
+//
+//  Created by ALLESTA MARTINEZ Cane on 28/10/2020.
+//  Copyright © 2020 ALLESTA MARTINEZ Cane. All rights reserved.
+//
+
+import UIKit
+
+// MARK: Para Gestionar las constraints en las Stack
+public struct AnchoredConstraints {
+    public var top, leading, bottom, trailing, width, height: NSLayoutConstraint?
+}
+public enum Anchor {
+    case top(_ top: NSLayoutYAxisAnchor, constant: CGFloat = 0)
+    case leading(_ leading: NSLayoutXAxisAnchor, constant: CGFloat = 0)
+    case bottom(_ bottom: NSLayoutYAxisAnchor, constant: CGFloat = 0)
+    case trailing(_ trailing: NSLayoutXAxisAnchor, constant: CGFloat = 0)
+    case height(_ constant: CGFloat)
+    case width(_ constant: CGFloat)
+}
+
+extension UIView {
+    // MARK: Para Gestionar las constraints de cualquier vista
+    func setAnchors(top: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, trailing: NSLayoutXAxisAnchor?, padding: UIEdgeInsets = .zero, size: CGSize = .zero) {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant: padding.top).isActive = true
+        }
+        
+        if let leading = leading {
+            leadingAnchor.constraint(equalTo: leading, constant: padding.left).isActive = true
+        }
+        
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -padding.bottom).isActive = true
+        }
+        
+        if let trailing = trailing {
+            trailingAnchor.constraint(equalTo: trailing, constant: -padding.right).isActive = true
+        }
+        
+        if size.width != 0 {
+            widthAnchor.constraint(equalToConstant: size.width).isActive = true
+        }
+        
+        if size.height != 0 {
+            heightAnchor.constraint(equalToConstant: size.height).isActive = true
+        }
+    }
+    // Para añadirle sombras a la vista, una pijada ;)
+    open func setShadow(opacity: Float = 0, radius: CGFloat = 0, offset: CGSize = .zero, color: UIColor = .black) {
+        layer.shadowOpacity = opacity
+        layer.shadowRadius = radius
+        layer.shadowOffset = offset
+        layer.shadowColor = color.cgColor
+    }
+    // MARK: Para crear una Vertical Stack
+    @discardableResult
+    open func VStack(_ views: UIView..., spacing: CGFloat = 0, alignment: UIStackView.Alignment = .fill, distribution: UIStackView.Distribution = .fill) -> UIStackView {
+        return _stack(.vertical, views: views, spacing: spacing, alignment: alignment, distribution: distribution)
+    }
+    // MARK: Para crear una Horizontal Stack
+    @discardableResult
+    open func HStack(_ views: UIView..., spacing: CGFloat = 0, alignment: UIStackView.Alignment = .fill, distribution: UIStackView.Distribution = .fill) -> UIStackView {
+        return _stack(.horizontal, views: views, spacing: spacing, alignment: alignment, distribution: distribution)
+    }
+    // MARK: Para separar el array de Stacks y gestionar los constraints de cada uno
+    fileprivate func _stack(_ axis: NSLayoutConstraint.Axis = .vertical, views: [UIView], spacing: CGFloat = 0, alignment: UIStackView.Alignment = .fill, distribution: UIStackView.Distribution = .fill) -> UIStackView {
+        let stackView = UIStackView(arrangedSubviews: views)
+        stackView.axis = axis
+        stackView.spacing = spacing
+        stackView.alignment = alignment
+        stackView.distribution = distribution
+        addSubview(stackView)
+        stackView.fillSuperview()
+        return stackView
+    }
+    
+    @discardableResult
+    open func fillSuperview(padding: UIEdgeInsets = .zero) -> AnchoredConstraints {
+        translatesAutoresizingMaskIntoConstraints = false
+        let anchoredConstraints = AnchoredConstraints()
+        guard let superviewTopAnchor = superview?.topAnchor,
+            let superviewBottomAnchor = superview?.bottomAnchor,
+            let superviewLeadingAnchor = superview?.leadingAnchor,
+            let superviewTrailingAnchor = superview?.trailingAnchor else {
+                return anchoredConstraints
+        }
+        
+        return anchor(top: superviewTopAnchor, leading: superviewLeadingAnchor, bottom: superviewBottomAnchor, trailing: superviewTrailingAnchor, padding: padding)
+    }
+    
+    @discardableResult
+    open func anchor(top: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, trailing: NSLayoutXAxisAnchor?, padding: UIEdgeInsets = .zero, size: CGSize = .zero) -> AnchoredConstraints {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        var anchoredConstraints = AnchoredConstraints()
+        
+        if let top = top {
+            anchoredConstraints.top = topAnchor.constraint(equalTo: top, constant: padding.top)
+        }
+        
+        if let leading = leading {
+            anchoredConstraints.leading = leadingAnchor.constraint(equalTo: leading, constant: padding.left)
+        }
+        
+        if let bottom = bottom {
+            anchoredConstraints.bottom = bottomAnchor.constraint(equalTo: bottom, constant: -padding.bottom)
+        }
+        
+        if let trailing = trailing {
+            anchoredConstraints.trailing = trailingAnchor.constraint(equalTo: trailing, constant: -padding.right)
+        }
+        
+        if size.width != 0 {
+            anchoredConstraints.width = widthAnchor.constraint(equalToConstant: size.width)
+        }
+        
+        if size.height != 0 {
+            anchoredConstraints.height = heightAnchor.constraint(equalToConstant: size.height)
+        }
+        
+        [anchoredConstraints.top, anchoredConstraints.leading, anchoredConstraints.bottom, anchoredConstraints.trailing, anchoredConstraints.width, anchoredConstraints.height].forEach{ $0?.isActive = true }
+        
+        return anchoredConstraints
+    }
+    // MARK: Para gestionar el size de una vista
+    @discardableResult
+    open func withSize<T: UIView>(_ size: CGSize) -> T {
+        translatesAutoresizingMaskIntoConstraints = false
+        widthAnchor.constraint(equalToConstant: size.width).isActive = true
+        heightAnchor.constraint(equalToConstant: size.height).isActive = true
+        return self as! T
+    }
+    // MARK: Para gestionar el height de una vista
+    @discardableResult
+    open func withHeight(_ height: CGFloat) -> UIView {
+        translatesAutoresizingMaskIntoConstraints = false
+        heightAnchor.constraint(equalToConstant: height).isActive = true
+        return self
+    }
+    // MARK: Para gestionar el width de una vista
+    @discardableResult
+    open func withWidth(_ width: CGFloat) -> UIView {
+        translatesAutoresizingMaskIntoConstraints = false
+        widthAnchor.constraint(equalToConstant: width).isActive = true
+        return self
+    }
+    // MARK: Para gestionar los bordes de una vista
+    @discardableResult
+    func withBorder<T: UIView>(width: CGFloat, color: UIColor) -> T {
+        layer.borderWidth = width
+        layer.borderColor = color.cgColor
+        return self as! T
+    }
+}
+ 
+// MARK: Para Añadirle margins y paddings a las Stacks
+extension UIStackView {
+    @discardableResult
+    open func withMargins(_ margins: UIEdgeInsets) -> UIStackView {
+        layoutMargins = margins
+        isLayoutMarginsRelativeArrangement = true
+        return self
+    }
+    
+    @discardableResult
+    open func padLeft(_ left: CGFloat) -> UIStackView {
+        isLayoutMarginsRelativeArrangement = true
+        layoutMargins.left = left
+        return self
+    }
+    
+    @discardableResult
+    open func padTop(_ top: CGFloat) -> UIStackView {
+        isLayoutMarginsRelativeArrangement = true
+        layoutMargins.top = top
+        return self
+    }
+    
+    @discardableResult
+    open func padBottom(_ bottom: CGFloat) -> UIStackView {
+        isLayoutMarginsRelativeArrangement = true
+        layoutMargins.bottom = bottom
+        return self
+    }
+    
+    @discardableResult
+    open func padRight(_ right: CGFloat) -> UIStackView {
+        isLayoutMarginsRelativeArrangement = true
+        layoutMargins.right = right
+        return self
+    }
+}
+
+// MARK: Para inicializar Labels
+extension UILabel {
+    convenience public init(text: String? = nil, font: UIFont? = UIFont.systemFont(ofSize: 14), textColor: UIColor = .black, textAlignment: NSTextAlignment = .left, numberOfLines: Int = 1) {
+        self.init()
+        self.text = text
+        self.font = font
+        self.textColor = textColor
+        self.textAlignment = textAlignment
+        self.numberOfLines = numberOfLines
+    }
+}
+
+// MARK: Para inicializar TextViews
+extension UITextView {
+    convenience public init(text: String?, font: UIFont? = UIFont.systemFont(ofSize: 14), textColor: UIColor = .black, textAlignment: NSTextAlignment = .left) {
+        self.init()
+        self.text = text
+        self.font = font
+        self.textColor = textColor
+        self.textAlignment = textAlignment
+    }
+}
+
+// MARK: Para poner los colores en RGBO
+extension UIColor {
+    static public func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+}
+
+// MARK: Se puede estudiar
+@available(iOS 11.0, tvOS 11.0, *)
+extension UIButton {
+    convenience public init(title: String, titleColor: UIColor, font: UIFont = .systemFont(ofSize: 14), backgroundColor: UIColor = .clear, target: Any? = nil, action: Selector? = nil) {
+        self.init(type: .system)
+        setTitle(title, for: .normal)
+        setTitleColor(titleColor, for: .normal)
+        self.titleLabel?.font = font
+        
+        self.backgroundColor = backgroundColor
+        if let action = action {
+            addTarget(target, action: action, for: .primaryActionTriggered)
+        }
+    }
+    
+    convenience public init(image: UIImage, tintColor: UIColor? = nil, target: Any? = nil, action: Selector? = nil) {
+        self.init(type: .system)
+        if tintColor == nil {
+            setImage(image, for: .normal)
+        } else {
+            setImage(image.withRenderingMode(.alwaysTemplate), for: .normal)
+            self.tintColor = tintColor
+        }
+        
+        if let action = action {
+            addTarget(target, action: action, for: .primaryActionTriggered)
+        }
+    }
+    
+}
+
+
+
